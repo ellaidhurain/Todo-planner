d3963e22b69e974787b66f7257a355c32d791a12
ellaidhurai n <ellaidhurai.97@gmail.com>
2023-04-28 17:10:59 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,3 +4,33 @@ export const theme = {
   extend: {},
 };
 export const plugins = [];
+
+// tailwind.config.js
+
+module.exports = {
+  darkMode: 'class', // enable the dark mode class
+  theme: {
+    extend: {
+      colors: {
+        'primary': {
+          light: '#6EE7B7',
+          DEFAULT: '#48BB78',
+          dark: '#38A169',
+        },
+        'secondary': {
+          light: '#FECACA',
+          DEFAULT: '#FCA5A5',
+          dark: '#F87171',
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['dark'],
+      borderColor: ['dark'],
+      textColor: ['dark'],
+    },
+  },
+  plugins: [],
+}
